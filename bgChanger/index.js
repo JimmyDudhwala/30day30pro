1399464daf4c5b46d0fdfd7fac1d7d549d63cb97
Jimmy <jimmydudhwala25@gmail.com>
2024-03-30 05:48:11 +0530

    javascript applied

--- bgChanger/index.js
+++ bgChanger/index.js
@@ -1,29 +1,23 @@
-const button = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button');
 const body = document.querySelector('body');
 
-
-
-button.forEach((button)=>{
-    console.log(button);
-
-    button.addEventListener("click",(e)=>{
-        console.log()
-        
-        if(e.target.class="red"){
-            body.style.backgroundColor=e.target.class;          
-        }
-       if(e.target.class="green"){
-            body.style.backgroundColor="green";
-            
-        }
-        if(e.target.class="blue"){
-            body.style.backgroundColor=e.target.class;
-           
-        }
-        if(e.target.class="erase"){
-            body.style.backgroundColor=e.target.class;
-           
-        }
-
-    })
-})
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'red') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'green') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'erase') {
+      body.style.backgroundColor = "lightcyan";
+    }
+    
+  });
+});
