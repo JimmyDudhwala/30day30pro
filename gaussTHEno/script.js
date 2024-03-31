44cbf29a2071762675d4a0faa7d1eb7f9520697a
Jimmy <jimmydudhwala25@gmail.com>
2024-04-01 01:32:20 +0530

    css and html done

--- gaussTHEno/script.js
+++ gaussTHEno/script.js
@@ -1,22 +1,3 @@
-const random =  (parseInt(Math.random()*100 +1));
+const random = parseInt(Math.random()*100 + 1);
 
-const guess = document.querySelector('#guess');
-const submit = document.querySelector('#sbt');
-
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    const value = parseInt(guess.value);
-    if (value === random) {
-        alert('Congratulations! You have guessed the number');
-    } else if (value > random) {
-        alert('Too high! Try again');
-    } else {
-        alert('Too low! Try again');
-    }
-    guess.value = '';
-
-
-
-}, false);
-
-
+const 
