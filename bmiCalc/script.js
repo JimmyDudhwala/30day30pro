51e1d7de32dd0b7c540c1c9ad72ad6fd7aa35e8e
Jimmy <jimmydudhwala25@gmail.com>
2024-04-02 12:53:11 +0530

    sound effect project started

--- bmiCalc/script.js
+++ bmiCalc/script.js
@@ -2,7 +2,7 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
-
+    
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const result = document.querySelector('#result')
@@ -24,7 +24,3 @@ form.addEventListener('submit',(e)=>{
     }
 
 })
-
-// <p>Under Weight = Less than 18.6</p>
-// <p>Normal Range = 18.6 and 24.9</p>
-// <p>Overweight = Greater than 24.9</p>
--- gaussTHEno/script.js
+++ gaussTHEno/script.js
@@ -1,3 +1,29 @@
 const random = parseInt(Math.random()*100 + 1);
 
-const 
+console.log(random);
+
+const submit = document.querySelector('#btn');
+const input = document.getElementById('pre');
+const advice = document.querySelector('#advice');
+
+
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const guess = input.value;
+    console.log(guess);
+    if(random == guess){
+        advice.innerHTML="<h3>you are right</h3>"
+        submit.remove();
+
+    }
+
+    else{
+        advice.innerHTML="<h3>you are wrong</h3>"
+        submit.remove();
+    }
+
+})
+
+
+
