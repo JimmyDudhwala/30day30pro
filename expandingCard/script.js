423ea194f442cecb404c246b5647b27ed7a6602e
Jimmy <jimmydudhwala25@gmail.com>
2024-04-05 21:35:52 +0530

    javascript is done for expandinf card

--- expandingCard/script.js
+++ expandingCard/script.js
@@ -1,11 +1,27 @@
+
 const panel = document.querySelectorAll('.panel');
+const body = document.querySelector('body');
+
+console.log(body);
 
 console.log(panel);
 
-panel.forEach(panel => {
+const removeActiveClasses = () => {
+    panel.forEach((panel) => {
+      panel.classList.remove("select");
+    });
+  };
+
+panel.forEach((panel) => {
+    panel.classList.remove('select');
     panel.addEventListener("click", (e) => {
-        if (!e.target.style.backgroundImage) {
-            console('yes')
-        }
+        if (e.target.style.backgroundImage) {
+            removeActiveClasses();
+            console.log(e.target.style.backgroundImage);
+            body.style.backgroundImage = e.target.style.backgroundImage;
+            panel.classList.add('select'); // Add class to the clicked panel
+
+            
+        } 
     });
 });
