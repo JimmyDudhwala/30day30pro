3ccd49d52c4345e510233ba48dec6254ba0b3e4b
Jimmy <jimmydudhwala25@gmail.com>
2024-04-06 17:45:52 +0530

    gauess the  no. is  done

--- gaussTHEno/script.js
+++ gaussTHEno/script.js
@@ -5,25 +5,40 @@ console.log(random);
 const submit = document.querySelector('#btn');
 const input = document.getElementById('pre');
 const advice = document.querySelector('#advice');
-
-
+const again = document.querySelector('.try')
 
 submit.addEventListener("click", (e) => {
     e.preventDefault();
     const guess = input.value;
     console.log(guess);
+    input.value='';
     if(random == guess){
         advice.innerHTML="<h3>you are right</h3>"
         submit.remove();
-
+        again.remove(); 
     }
 
     else{
         advice.innerHTML="<h3>you are wrong</h3>"
-        submit.remove();
+        retry();
+        again.addEventListener('click', () => {
+            advice.remove();
+            again.remove();     
+        })
     }
-
 })
+
+function retry(){
+
+    again.innerHTML = "<h3>DO IT AGAIN</h3>"
+    again.classList.add('again');  
+
+}
+
+
+
+
+
 
 
 
