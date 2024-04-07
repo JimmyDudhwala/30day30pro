66e68d52eef1528132f68c59ae991dee42873b2a
Jimmy <jimmydudhwala25@gmail.com>
2024-04-08 02:51:53 +0530

    scroll animation is added

---
+++ scroll+fixedAnimation/script.js
@@ -0,0 +1,43 @@
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+console.log(box1);
+console.log(window.scrollY);
+window.addEventListener('scroll', function() {
+    const box1 = document.querySelector('.box1');
+
+    if (window.scrollY > window.innerHeight / 2) {
+       box1.classList.add('show');
+    } else {
+        box1.classList.remove('show');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    const box2 = document.querySelector('.box2');
+
+    if (window.scrollY > (window.innerHeight) / 2) {
+       box2.classList.add('show');
+    } else {
+        box2.classList.remove('show');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    const box3 = document.querySelector('.box3');
+
+    if (window.scrollY > 2*(window.innerHeight) / 3) {
+       box3.classList.add('show');
+    } else {
+        box3.classList.remove('show');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    const box4 = document.querySelector('.box4');
+
+    if (window.scrollY > 2*(window.innerHeight) / 3) {
+       box4.classList.add('show');
+    } else {
+        box4.classList.remove('show');
+    } 
+});
