81f26ee239691873adef375eb3564c3e50283d36
Jimmy <jimmydudhwala25@gmail.com>
2024-04-10 19:36:45 +0530

    new start

--- scroll+fixedAnimation/script.js
+++ scroll+fixedAnimation/script.js
@@ -1,15 +1,17 @@
+
+
 const box1 = document.querySelector('.box1');
 const box2 = document.querySelector('.box2');
 console.log(box1);
 console.log(window.scrollY);
 window.addEventListener('scroll', function() {
-    const box1 = document.querySelector('.box1');
+        const box1 = document.querySelector('.box1');
 
-    if (window.scrollY > window.innerHeight / 2) {
-       box1.classList.add('show');
-    } else {
-        box1.classList.remove('show');
-    }
+        if (window.scrollY > window.innerHeight / 2) {
+             box1.classList.add('show');
+        } else {
+                box1.classList.remove('show');
+        }
 });
 
 window.addEventListener('scroll', function() {
@@ -40,4 +42,37 @@ window.addEventListener('scroll', function() {
     } else {
         box4.classList.remove('show');
     } 
+});
+
+const drag = document.querySelector('.drag');
+
+console.log(drag);
+
+let press = false;
+let startX = 0;
+
+drag.addEventListener('mousedown', (e) => {
+    press = true;
+    startX = e.clientX;
+    console.log(startX);
+    drag.style.cursor = 'grabbing';
+});
+
+drag.addEventListener('mouseup', (e) => {
+     press = false;
+     drag.style.cursor = 'grab';
+});
+
+drag.addEventListener('mouseleave', (e) => {
+     press = false;
+});
+
+drag.addEventListener('mousemove', (e) => {
+     if (!press) {
+            return;
+     }
+     e.preventDefault();
+     drag.scrollLeft += startX - e.clientX;
+     console.log(e.clientX);
+     
 });
